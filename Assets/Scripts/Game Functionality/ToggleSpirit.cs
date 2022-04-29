084a1b4dff76382da983e32802fc7a07b724dcda
Logan D'Auria <lxd1644@rit.edu>
2022-04-28 21:34:53 -0400

    detailed docs

--- Assets/Scripts/Game Functionality/ToggleSpirit.cs
+++ Assets/Scripts/Game Functionality/ToggleSpirit.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Used to change the inner orb into a particle person
 public class ToggleSpirit : MonoBehaviour
 {
     public GameObject spirit;
     public GameObject sphere2;
 
+    /// <summary>
+    /// Called with a unity event to toggle off inner sphere, toggle on spirit
+    /// </summary>
     public void Toggle()
     {
         spirit.SetActive(true);
